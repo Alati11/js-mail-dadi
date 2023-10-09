d92009c22dcc0ef890f55234249625d0f2849e03
Alati11 <peppealati@gmail.com>
2023-10-09 16:31:13 +0200

    console-log mail corretta

--- js/app.js
+++ js/app.js
@@ -2,9 +2,33 @@
 
 // - Chiedi all’utente la sua email
 //     - creare un' array contenente una lista di le mail valide per l'accesso
-//     - tramite comanbdo prompt chiedere all'utente di inserire la mail
-// - Controlla che sia nella lista di chi può accedere
-//     - usare il ciclo for per confrontare la mail inserita con quelle esistenti
+
+const arrayMail = [
+    'giuseppe@gmail.com',
+    'alice@gmail.com',
+    'alessandra@gmail.com',
+    'ciccio@gmail.com'
+]
+console.log(arrayMail)
+
+// - tramite comanbdo prompt chiedere all'utente di inserire la mail
+let mailUser = prompt('Inserisci mail');
+
+// - usare il ciclo for per confrontare la mail inserita con quelle esistenti
+const lunghezzaArray = arrayMail.length
+
+// - Controlla che sia nella lista di chi pu
+for (let i = 0; i < lunghezzaArray; i++){
+    mailCurrent = arrayMail [i]
+    console.log(i, mailCurrent); 
+}
+
 // - Stampa un messaggio appropriato sull’esito del controllo
 //     - Se mail è corrispondente stampare: mail corretta.
 //     - altrimenti stampare messaggio: mail errata. 
+if ( mailUser === mailCurrent){
+    console.log('Mail corretta');
+   } else {
+    console.log('Mail errata');
+    }
+      
