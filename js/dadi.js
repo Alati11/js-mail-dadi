8ce834ca1489cf6a92dfe63298ce428e32225295
Alati11 <peppealati@gmail.com>
2023-10-10 01:28:54 +0200

    messaggio risultato

--- js/dadi.js
+++ js/dadi.js
@@ -2,14 +2,45 @@
 
 // - Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // - Stabilire il vincitore, in base a chi fa il punteggio più alto.
-
+let numeroRandomGiocatore  
+let numeroRandomBanco
+let risultatoGiocatore
+let risultatoBanco 
 
 btnLanciaDadi = document.getElementById("btn_lancia");
 
 btnLanciaDadi.addEventListener("click", function () {
 
     numeroRandomGiocatore = Math.round(Math.random() * (6 - 1) + 1);
-    //console.log(numeroRandomGiocatore);
+    console.log(numeroRandomGiocatore);
     
     numeroRandomBanco = Math.round(Math.random() * (6 - 1) + 1);
-    //console.log(numeroRandomBanco);
+    console.log(numeroRandomBanco);
+
+    if (numeroRandomGiocatore > numeroRandomBanco) {
+        risultatoGiocatore = "Grande!!! Hai vinto!!!"
+            console.log(risultatoGiocatore);
+    } else if (numeroRandomGiocatore === numeroRandomBanco){
+        risultatoGiocatore = "Pareggio... riproviamo!";
+        // risultatoBanco = "Pareggio... riproviamo!"
+        // console.log(risultatoGiocatore);  
+    } else {
+        risultatoGiocatore = "Hai perso! Peccatooooo... Riprova!!!"
+        console.log(risultatoGiocatore); 
+    }
+       
+    
+        // risultatoBanco = "Hai vinto!";
+        // console.log(risultatoGiocatore);
+
+        numGiocatore = document.getElementById("num-giocatore");
+        numBanco = document.getElementById("num-banco");
+        messaggio = document.getElementById("messaggio")
+        
+        
+        numGiocatore.innerHTML = numeroRandomGiocatore;
+        numBanco.innerHTML = numeroRandomBanco;
+        messaggio.innerHTML = risultatoGiocatore
+  //console.log(valoreGiocatore);
+                
+})          
